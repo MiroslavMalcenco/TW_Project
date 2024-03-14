a5f7de7a1a2962c7a1688f112a51874c3070ad78
Bogdan Mihailov <bogdanmihailov12@gmail.com>
2024-03-14 23:40:06 +0200

    Начал работать над DBModel. Добавил первый cs файл.

---
+++ Gamma.BusinessLogic/DBModel/UserContext.cs
@@ -0,0 +1,15 @@
+﻿using Gamma.Domain.Entities.User;
+using System.Data.Entity;
+
+namespace Gamma.BusinessLogic.DBModel
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(string connectionString)
+        {
+            Database.Connection.ConnectionString = connectionString;
+        }
+        public UserContext() : base("name=Gamma") { }
+        public virtual DbSet<UDBModel> Users { get; set; }
+    }
+}
