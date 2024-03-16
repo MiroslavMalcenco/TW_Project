1787ccb452dd5c162ad717ce8a7180cb41e157fb
Bogdan Mihailov <bogdanmihailov12@gmail.com>
2024-03-16 23:42:10 +0200

    Дополнил DBModel файлом для создания обзора

---
+++ Gamma.BusinessLogic/DBModel/PostContext.cs
@@ -0,0 +1,11 @@
+﻿using Gamma.Domain.Entities.Post;
+using System.Data.Entity;
+
+namespace Gamma.BusinessLogic.DBModel
+{
+    public class PostContext : DbContext
+    {
+        public PostContext() : base("name=Gamma") { }
+        public virtual DbSet<PDbModel> Posts { get; set; }
+    }
+}
