1330e1140b68c1915d53f2e4888eb8acd6240e8c
Bogdan Mihailov <bogdanmihailov12@gmail.com>
2024-03-17 23:43:11 +0200

    Добавил файл в DBModel для настройки сессии пользователя

---
+++ Gamma.BusinessLogic/DBModel/SessionContext.cs
@@ -0,0 +1,11 @@
+﻿using Gamma.Domain.Entities.Session;
+using System.Data.Entity;
+
+namespace Gamma.BusinessLogic.DBModel
+{
+    public class SessionContext : DbContext
+    {
+        public SessionContext() : base("name=Gamma") { }
+        public virtual DbSet<SDBModel> Sessions { get; set; }
+    }
+}
