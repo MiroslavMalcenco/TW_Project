c68f749cd2630d6da071d6ba942f1891720edd31
Bogdan Mihailov <bogdanmihailov12@gmail.com>
2024-03-18 23:45:30 +0200

    Создал логику для обзора. Внес некоторые изменения в структуру бизнес-логики.

---
+++ Gamma.BusinessLogic/PostBL.cs
@@ -0,0 +1,72 @@
+﻿using Gamma.BusinessLogic.Core;
+using Gamma.BusinessLogic.DBModel;
+using Gamma.BusinessLogic.Interfaces;
+using Gamma.Domain.Entities.Post;
+using Gamma.Domain.Entities.Response;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace Gamma.BusinessLogic
+{
+    public class PostBL : PostApi, IPost
+    {
+        private readonly PostContext _context;
+        public PostBL()
+        {
+            _context = new PostContext();
+        }
+        public PostBL(PostContext context)
+        {
+            _context = context;
+        }
+        public ServiceResponse AddPostAction(PDbModel Name)
+        {
+            return AddPost(Name);
+        }
+        public PDbModel GetById(int PostID)
+        {
+            return _context.Posts.Find(PostID);
+        }
+        public IEnumerable<PDbModel> GetAll()
+        {
+            return _context.Posts.ToList();
+        }
+        public IEnumerable<PostMinimal> GetBySearchWrapData(PSearchWrapData searchWrapData)
+        { 
+            return ReturnPostsBySearchWrapData(searchWrapData);
+        }
+        public IEnumerable<PostMinimal> GetPostsByIngredientsOrCuisine(string Ingredients)
+        {
+            return ReturnPostsByIngredientsOrCuisine(Ingredients);
+        }
+        public void Update(PDbModel Name)
+        {
+            _context.Entry(Name).State = EntityState.Modified;
+        }
+        public void Delete(int PostID)
+        {
+            PDbModel Name = _context.Posts.Find(PostID);
+            if (Name != null)
+            {
+                _context.Posts.Remove(Name);
+            }
+        }
+        public IEnumerable<PostMinimal> GetLatestPosts()
+        {
+            return ReturnLatestPosts();
+        }
+        public IEnumerable<PostMinimal> GetPostsByAuthor(string author)
+        {
+            return ReturnPostsByAuthor(author);
+        }
+        public IEnumerable<PostMinimal> GetPostsByListingFilter(PListingFilterData data)
+        {
+            return ReturnPostsByListingFilter(data);
+        }
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ Gamma.BusinessLogic/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("Gamma.BusinessLogic")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Gamma.BusinessLogic")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("8c1d9b1e-c560-4e60-8f4f-4056ffe5a0e5")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
