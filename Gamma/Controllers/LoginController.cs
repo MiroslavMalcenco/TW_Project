fb5e0b2d1524a7aef898af596ece749d9a841071
Miroslav Malcenco <miroslav.malcenco@isa.utm.md>
2024-03-24 12:33:11 +0200

    Настройка BL

---
+++ Gamma/Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.SessionState;
+
+namespace Gamma.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly ISessionIDManager _session;
+        public LoginController()
+        {
+            var bl = new BussinesLogic();
+            _session = new ISessionIDManager();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
