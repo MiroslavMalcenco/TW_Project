738d60d574e9445eb0c4989bae5b5860a10914b2
MihailGanea <mihail.ganea@isa.utm.md>
2024-03-26 00:26:40 +0200

    Добавил View и контроллеры для раздела сайта "О нас"

---
+++ Gamma.Web/Controllers/AboutUsController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+
+namespace Gamma.Web.Controllers
+{
+    public class AboutUsController : BaseController
+    {
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+        public ActionResult ContactUs()
+        {
+            return View();
+        }
+        public ActionResult FAQs()
+        {
+            return View();
+        }
+    }
+}
---
+++ Gamma.Web/Controllers/ContactUsController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Gamma.Web.Controllers
+{
+    public class ContactUsController : BaseController
+    {
+        public ActionResult ContactUs()
+        {
+            return View();
+        }
+    }
+}
