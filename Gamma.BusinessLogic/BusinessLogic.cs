226832a8cf93b9e5c625808525911c091cb4ff27
Bogdan Mihailov <bogdanmihailov12@gmail.com>
2024-04-24 23:48:25 +0300

    Создал основную бизнес-логику для всего проекта

---
+++ Gamma.BusinessLogic/BusinessLogic.cs
@@ -0,0 +1,16 @@
+﻿using Gamma.BusinessLogic.Interfaces;
+
+namespace Gamma.BusinessLogic
+{
+    public class BusinessLogic
+    {
+        public ISession GetSessionBL()
+        {
+            return new SessionBL();
+        }
+        public IPost GetPostBL()
+        {
+            return new PostBL();
+        }
+    }
+}
