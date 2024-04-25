55e6f8460db755d70fd3f37bbf9384ff142aab5f
Bogdan Mihailov <bogdanmihailov12@gmail.com>
2024-04-25 23:49:45 +0300

    Написал логику для интерфейса обзора

---
+++ Gamma.BusinessLogic/Interfaces/IPost.cs
@@ -0,0 +1,21 @@
+﻿using Gamma.Domain.Entities.Post;
+using Gamma.Domain.Entities.Response;
+using System.Collections.Generic;
+
+namespace Gamma.BusinessLogic.Interfaces
+{
+    public interface IPost
+    {
+        ServiceResponse AddPostAction(PDbModel Name);
+        PDbModel GetById(int postID);
+        IEnumerable<PDbModel> GetAll();
+        IEnumerable<PostMinimal> GetBySearchWrapData(PSearchWrapData searchWrapData);
+        IEnumerable<PostMinimal> GetPostsByIngredientsOrCuisine(string Ingredients);
+        IEnumerable<PostMinimal> GetPostsByAuthor(string author);
+        IEnumerable<PostMinimal> GetLatestPosts();
+        IEnumerable<PostMinimal> GetPostsByListingFilter(PListingFilterData data);
+        void Update(PDbModel Name);
+        void Delete(int PostID);
+        void Save();
+    }
+}
