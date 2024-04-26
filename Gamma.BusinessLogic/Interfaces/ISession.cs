26b441067a9304c50a466cec62a6ed69693ce914
Bogdan Mihailov <bogdanmihailov12@gmail.com>
2024-04-26 23:50:49 +0300

    Создал файл, описывающий логику для интерфейса сессии пользователя

---
+++ Gamma.BusinessLogic/Interfaces/ISession.cs
@@ -0,0 +1,18 @@
+﻿using Gamma.Domain.Entities.Response;
+using Gamma.Domain.Entities.User;
+using System.Collections.Generic;
+
+namespace Gamma.BusinessLogic.Interfaces
+{
+    public interface ISession
+    {
+        IEnumerable<UDBModel> GetAll();
+        ServiceResponse ValidateUserCredential(ULoginData user);
+        ServiceResponse ChangePassword(UChangePasswordData password);
+        ServiceResponse ValidateUserRegister(URegisterData newUser);
+        CookieResponse GenCookie(string username);
+        UserMinimal GetUserByCookie(string apiCookieValue);
+        ServiceResponse EditProfileAction(UEditProfileData existingUser);
+        UEditProfileData GetUserById(int userId);
+    }
+}
