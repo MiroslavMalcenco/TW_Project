bfb61f50699e50ed6911e81c65a8151f59b2fe52
Bogdan Mihailov <bogdanmihailov12@gmail.com>
2024-04-28 23:55:41 +0300

    Создал файл SDbModels для корректной реализации сессии пользователя

---
+++ Gamma.Domain/Session/SDbModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Gamma.Domain.Entities.Session
+{
+    public class SDBModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int SessionId { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string Username { get; set; }
+        [Required]
+        public string CookieString { get; set; }
+        [Required]
+        public DateTime ExpireTime { get; set; }
+    }
+}
