0e3cd64663976c024fa68c3e3c8ffcbbc5b573f8
Bogdan Mihailov <bogdanmihailov12@gmail.com>
2024-04-29 23:57:08 +0300

    Немного поработал с cookie. Добавил файл ответа для данного функционала.

---
+++ Gamma.Domain/Response/CookieResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Web;
+
+namespace Gamma.Domain.Entities.Response
+{
+     public class CookieResponse
+     {
+          public DateTime Data { get; set; } 
+          public HttpCookie Cookie { get; set; }
+     }
+}
