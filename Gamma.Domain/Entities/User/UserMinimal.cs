08e1521795d87faea7da9f340ff1a19fd26c402b
Bogdan Mihailov <bogdanmihailov12@gmail.com>
2024-05-07 00:06:26 +0300

    Добавил минимальные необходимые параметры для пользователя

---
+++ Gamma.Domain/Entities/User/UserMinimal.cs
@@ -0,0 +1,15 @@
+﻿using Gamma.Domain.Enum;
+using System;
+
+namespace Gamma.Domain.Entities.User
+{
+    public class UserMinimal
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public DateTime LastLogin { get; set; }
+        public string LasIp { get; set; }
+        public URole Level { get; set; }
+    }
+}
