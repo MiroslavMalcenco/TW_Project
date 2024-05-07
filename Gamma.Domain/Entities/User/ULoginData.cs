636bbd14ab3395b111d25e5dc87b9d309edf9836
Bogdan Mihailov <bogdanmihailov12@gmail.com>
2024-05-08 00:07:19 +0300

    Добавил файлы для логина и регистрации пользователя

---
+++ Gamma.Domain/Entities/User/ULoginData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Gamma.Domain.Entities.User
+{
+    public class ULoginData
+    {
+        public string IP { get; set; }
+        public DateTime LastLoginTime { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Gamma.Domain/Entities/User/URegisterData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Gamma.Domain.Entities.User
+{
+    public class URegisterData
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+        public string UserName { get; set; }
+        public bool Terms { get; set; }
+        public string IP { get; set; }
+        public DateTime RegisterDateTime { get; set; }
+    }
+}
