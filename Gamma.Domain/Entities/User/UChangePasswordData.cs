7236998ba64f8f63af01d9b204076980d3ef919b
Bogdan Mihailov <bogdanmihailov12@gmail.com>
2024-05-12 00:09:31 +0300

    Поработал над профилем пользователя и реализовал смену пароля

---
+++ Gamma.Domain/Entities/User/UChangePasswordData.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gamma.Domain.Entities.User
+{
+    public class UChangePasswordData
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Введите старый пароль")]
+        [DataType(DataType.Password)]
+        [StringLength(50, MinimumLength = 8, ErrorMessage = "Пароль слишком короткий")]
+        public string OldPassword { get; set; }
+        [Required(ErrorMessage = "Введите новый пароль")]
+        [DataType(DataType.Password)]
+        [StringLength(50, MinimumLength = 8, ErrorMessage = "Пароль слишком короткий")]
+        public string NewPassword { get; set; }
+        [Required(ErrorMessage = "Подтвердите новый пароль")]
+        [DataType(DataType.Password)]
+        [StringLength(50, MinimumLength = 8, ErrorMessage = "Пароль слишком короткий")]
+        public string ConfirmedPassword { get; set; }
+    }
+}
