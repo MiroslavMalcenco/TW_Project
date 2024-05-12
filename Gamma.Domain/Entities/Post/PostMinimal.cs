586da6fd4ad7276d6fa7492f1705492f1d13d9ab
Bogdan Mihailov <bogdanmihailov12@gmail.com>
2024-05-13 00:10:36 +0300

    Создал файл с минимальными параметрами для создания и отображения обзора

---
+++ Gamma.Domain/Entities/Post/PostMinimal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Gamma.Domain.Entities.Post
+{
+    public class PostMinimal
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Ingredients { get; set; }
+        public int Calories { get; set; }
+        public int SpicinessLevel { get; set; }
+        public string Cuisine { get; set; }
+        public string DietaryRestrictions { get; set; }
+        public int Rating { get; set; }
+        public string ChefSpeciality { get; set; }
+        public int Price { get; set; }
+        public DateTime DateAdded { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
