55a26eaa2ed35ab3a6615af0658306c45a0cbb8e
Bogdan Mihailov <bogdanmihailov12@gmail.com>
2024-05-25 00:12:42 +0300

    Реализовал список для фильтра и поиск по данным для обзоров

---
+++ Gamma.Domain/Entities/Post/PListingFilterData.cs
@@ -0,0 +1,15 @@
+﻿namespace Gamma.Domain.Entities.Post
+{
+    public class PListingFilterData
+    {
+        public string KeyWord { get; set; }
+        public int MinPrice { get; set; }
+        public int MaxPrice { get; set; }
+        public string Cuisine { get; set; }
+        public int MinCalories { get; set; }
+        public int MaxCalories { get; set; }
+        public string Ingredients { get; set; }
+        public string ChefSpeciality { get; set; }
+        public string DietaryRestrictions { get; set; }
+    }
+}
---
+++ Gamma.Domain/Entities/Post/PSearchWrapData.cs
@@ -0,0 +1,9 @@
+﻿namespace Gamma.Domain.Entities.Post
+{
+    public class PSearchWrapData
+    {
+        public string IngredientsOrName { get; set; }
+        public string PriceRange { get; set; }
+        public string Cuisine { get; set; }
+    }
+}
