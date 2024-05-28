92a7690a97e729d32f149154f29a9aca23e661a5
Miroslav Malcenco <miroslav.malcenco@isa.utm.md>
2024-05-28 15:18:34 +0300

    Добавление

---
+++ Gamma.Helpers/LoginHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Gamma.Helpers
+{
+    public class LoginHelper
+    {
+        public static string HashGen(string password)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            var originalBytes = Encoding.Default.GetBytes(password + "twutm2024");
+            var encodedBytes = md5.ComputeHash(originalBytes);
+            return BitConverter.ToString(encodedBytes).Replace("-", "").ToLower();
+        }
+    }
+}
